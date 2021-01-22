cbb0ecfd733e1c490d93b47a09e43d62846900e2
Patrick <patrick.chardavoine@gmail.com>
2021-01-22 14:44:55 +0100

    file type : jpg and png only should be recorded!

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -15,7 +15,13 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    if (!extension) {
+      //console.log('extension '+extension)
+      callback(null,'');
+    }
+    else {
+      callback(null, name + Date.now() + '.' + extension);
+    }
   }
 });
 
